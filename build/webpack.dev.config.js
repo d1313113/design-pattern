4780c689be194e50ea7a993cfc1228a971b43164
cumelmell <cumelmell@foxmail.com>
2019-10-27 05:19:11 +0800

    feat(babel): add babel plugin
    - add babel-core & babel-loader & babel-preset-env
    - configure webpack to use babel-loader

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -7,6 +7,15 @@ module.exports = {
     path: __dirname,
     filename: './dist/bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
