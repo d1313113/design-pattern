781c90190ff60bc0bff1b19b7a8bb0716cbe69f8
cumelmell <cumelmell@foxmail.com>
2019-10-31 23:56:58 +0800

    chore: configure eslint & gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'semi': [2, 'always'],
-    'space-before-function-paren': ['error', 'never']
+    'space-before-function-paren': ['error', 'never'],
+    'eol-last': ['error', 'always']
   }
 };
