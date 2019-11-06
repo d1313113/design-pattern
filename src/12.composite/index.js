b2d8ca476dca128195ce665f39e05337a446c237
cumelmell <cumelmell@foxmail.com>
2019-11-06 22:26:43 +0800

    feat(composite): add composite pattern

---
+++ src/12.composite/index.js
@@ -0,0 +1,19 @@
+let vnode = {
+  tag: 'div',
+  attr: {
+    id: 'div1',
+    className: 'container'
+  },
+  children: [
+    {
+      tag: 'p',
+      attr: {},
+      children: [123]
+    },
+    {
+      tag: 'p',
+      attr: {},
+      children: [567]
+    }
+  ]
+};
